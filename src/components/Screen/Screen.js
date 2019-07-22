edbca9a1385dc72076f48973bdcc60f9777bcb15
Klimenko Ivan <kli-van@yandex.ru>
2019-07-22 19:31:05 +0300

    animation was made

--- src/components/Screen/Screen.js
+++ src/components/Screen/Screen.js
@@ -1,23 +1,122 @@
 import React, { PureComponent } from "react";
-import styleText from "./styleText.js";
 import "./Screen.css";
+import rawStyleText from "./rawStyleText";
 
 class Screen extends PureComponent {
-  state = {};
+  screenRef = React.createRef();
+
+  state = {
+    styleTextBuffer: "",
+    styleText: "",
+    markupText: ""
+  };
+
+  async writeNextChar(rawStyleText, options) {
+    let { charIndex, timeInterval } = options;
+    let { styleText, markupText, styleTextBuffer } = this.state;
+
+    let newCharIndex = charIndex + 1;
+    let char = rawStyleText.slice(charIndex, newCharIndex);
+
+    let styleCurrentState = this.processStyleText(
+      styleText,
+      styleTextBuffer,
+      char
+    );
+    styleText = styleCurrentState.styleText;
+    styleTextBuffer = styleCurrentState.styleTextBuffer;
+
+    // markupText += char;
+    markupText = this.processMarkupText(markupText, char);
+    await this.setState({
+      styleTextBuffer,
+      styleText,
+      markupText
+    });
+
+    if (newCharIndex < rawStyleText.length) {
+      await new Promise(resolve => setTimeout(() => resolve(), timeInterval));
+
+      this.writeNextChar(rawStyleText, {
+        charIndex: newCharIndex,
+        timeInterval
+      });
+    }
+  }
+
+  processMarkupText(markupText, char) {
+    let selectorRegexp = /(.*)$/;
+    let keyRegexp = /([a-zA-Z- ^\n]*)$/;
+    let valueRegexp = /([^:]*)$/;
+    let commentRegexp = /(\/\*(?:[^](?!\/\*))*\*\/)$/g;
+
+    if (char === "{") {
+      markupText = markupText.replace(
+        selectorRegexp,
+        '<span class="selector">$1</span>'
+      );
+      markupText += char;
+    } else if (char === ":") {
+      markupText = markupText.replace(keyRegexp, '<span class="key">$1</span>');
+      markupText += char;
+    } else if (char === ";") {
+      markupText = markupText.replace(
+        valueRegexp,
+        '<span class="value">$1</span>'
+      );
+      markupText += char;
+    } else if (char === "/") {
+      markupText += char;
+      markupText = markupText.replace(
+        commentRegexp,
+        '<span class="comment">$1</span>'
+      );
+    } else {
+      markupText += char;
+    }
+
+    return markupText;
+  }
+
+  processStyleText(styleText, styleTextBuffer, char) {
+    styleTextBuffer += char;
+
+    if (char === ";") {
+      styleText += styleTextBuffer;
+      styleTextBuffer = "";
+    }
+
+    return {
+      styleText,
+      styleTextBuffer
+    };
+  }
+
+  componentDidMount() {
+    this.writeNextChar(rawStyleText, {
+      charIndex: 0,
+      timeInterval: 50
+    });
+  }
+  componentDidUpdate() {
+    let element = this.screenRef.current;
+    element.scrollTop = element.scrollHeight;
+  }
 
   render() {
+    const { styleText, markupText } = this.state;
+
     return (
-      <div className="Screen">
-        {/* Привет. Меня зовут Клименко Иван, и я начинающий фронтенд-разработчик. */}
-        <div dangerouslySetInnerHTML={createMarkup()} />
+      <div ref={this.screenRef} className="Screen">
         <style>{styleText}</style>
+        <div id="screen" dangerouslySetInnerHTML={createMarkup(markupText)} />
       </div>
     );
   }
 }
 
-function createMarkup() {
-  return { __html: styleText };
+function createMarkup(rawMarkup) {
+  return { __html: rawMarkup };
 }
 
 export default Screen;
