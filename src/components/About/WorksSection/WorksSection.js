da03b9dea8272abc31cff38250168e2fff2a2561
Glacial <kli-van@yandex.ru>
2019-08-23 22:11:32 +0300

    add tiny-slider react

--- src/components/About/WorksSection/WorksSection.js
+++ src/components/About/WorksSection/WorksSection.js
@@ -1,12 +1,40 @@
 import React from "react";
 import styles from "./WorksSection.module.scss";
 import HeaderStub from "../../Header/HeaderStub";
+import TinySlider from "tiny-slider-react";
 
 function WorksSection() {
+  let imgs = [
+    "http://d2ji2mue1p384z.cloudfront.net/img/480x360/162951.jpg",
+    "http://d2ji2mue1p384z.cloudfront.net/img/480x360/162465.jpg",
+    "http://d2ji2mue1p384z.cloudfront.net/img/480x360/220048.jpg"
+  ];
+  const loadingImage =
+    "data:image/gif;base64,\
+  R0lGODlhAQABAPAAAMzMzAAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==";
+  const settings = {
+    lazyload: true,
+    nav: true,
+    // mouseDrag: true,
+    loop: true,
+    items: 1
+  };
+
   return (
     <section className={styles.hello}>
       <HeaderStub />
-      <div />
+      <TinySlider settings={settings}>
+        {imgs.map((el, index) => (
+          <div key={index} style={{ position: "relative" }}>
+            <img
+              className={`tns-lazy-img`}
+              src={loadingImage}
+              data-src={el}
+              alt=""
+            />
+          </div>
+        ))}
+      </TinySlider>
     </section>
   );
 }
