d838c07d479d298ba5d37c146307349bbd5bd422
Glacial <kli-van@yandex.ru>
2019-08-24 16:43:03 +0300

    works slider finished

--- src/components/About/WorksSection/WorksSection.js
+++ src/components/About/WorksSection/WorksSection.js
@@ -19,7 +19,7 @@ function WorksSection() {
         <div className={styles.works_header}>
           <FormattedMessage id="works.header" />
         </div>
-        <div className={`${styles.slider_wrapper} slider_wrapper`}>
+        <div className={`${styles.slider_works_wrapper} slider_works_wrapper`}>
           <SimpleSlider />
         </div>
       </div>
@@ -31,27 +31,54 @@ export default WorksSection;
 
 class SimpleSlider extends React.Component {
   render() {
-    var settings = {
+    const settings = {
       dots: true,
       infinite: false,
       speed: 500,
       slidesToShow: 1,
       slidesToScroll: 1
     };
+
+    const worksList = [
+      {
+        img: img1,
+        header: "Название работы",
+        text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed molestie sollicitudin urna, non molestie metus consectetur quis. Morbi feugiat lectus vel leo interdum, in commodo sapien vulputate. Integer orci turpis, porttitor id massa ut, luctus volutpat nisl. Mauris nulla est, interdum at placerat id, imperdiet in enim. Vestibulum rhoncus tincidunt semper. Aliquam efficitur diam sit amet placerat tincidunt. Donec sem magna, venenatis vitae nibh vitae, imperdiet gravida nulla. Nullam est ipsum, volutpat sed egestas non, blandit et turpis. Nulla viverra lectus at felis fermentum luctus. Proin porta ante sit amet justo fermentum faucibus. Aliquam bibendum consectetur ligula, quis consectetur est tincidunt non.`,
+        href: "/"
+      },
+      {
+        img: img2,
+        header: "Название работы",
+        text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed molestie sollicitudin urna, non molestie metus consectetur quis. Morbi feugiat lectus vel leo interdum, in commodo sapien vulputate. Integer orci turpis, porttitor id massa ut, luctus volutpat nisl. Mauris nulla est, interdum at placerat id, imperdiet in enim. Vestibulum rhoncus tincidunt semper. Aliquam efficitur diam sit amet placerat tincidunt. Donec sem magna, venenatis vitae nibh vitae, imperdiet gravida nulla. Nullam est ipsum, volutpat sed egestas non, blandit et turpis. Nulla viverra lectus at felis fermentum luctus. Proin porta ante sit amet justo fermentum faucibus. Aliquam bibendum consectetur ligula, quis consectetur est tincidunt non.`,
+        href: "/"
+      },
+      {
+        img: img3,
+        header: "Название работы",
+        text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed molestie sollicitudin urna, non molestie metus consectetur quis. Morbi feugiat lectus vel leo interdum, in commodo sapien vulputate. Integer orci turpis, porttitor id massa ut, luctus volutpat nisl. Mauris nulla est, interdum at placerat id, imperdiet in enim. Vestibulum rhoncus tincidunt semper. Aliquam efficitur diam sit amet placerat tincidunt. Donec sem magna, venenatis vitae nibh vitae, imperdiet gravida nulla. Nullam est ipsum, volutpat sed egestas non, blandit et turpis. Nulla viverra lectus at felis fermentum luctus. Proin porta ante sit amet justo fermentum faucibus. Aliquam bibendum consectetur ligula, quis consectetur est tincidunt non.`,
+        href: "/"
+      },
+      {
+        img: img4,
+        header: "Название работы",
+        text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed molestie sollicitudin urna, non molestie metus consectetur quis. Morbi feugiat lectus vel leo interdum, in commodo sapien vulputate. Integer orci turpis, porttitor id massa ut, luctus volutpat nisl. Mauris nulla est, interdum at placerat id, imperdiet in enim. Vestibulum rhoncus tincidunt semper. Aliquam efficitur diam sit amet placerat tincidunt. Donec sem magna, venenatis vitae nibh vitae, imperdiet gravida nulla. Nullam est ipsum, volutpat sed egestas non, blandit et turpis. Nulla viverra lectus at felis fermentum luctus. Proin porta ante sit amet justo fermentum faucibus. Aliquam bibendum consectetur ligula, quis consectetur est tincidunt non.`,
+        href: "/"
+      }
+    ];
+
     return (
       <Slider {...settings}>
-        <div>
-          <img src={img1} className={styles.slider_image} alt="" />
-        </div>
-        <div>
-          <img src={img2} className={styles.slider_image} alt="" />
-        </div>
-        <div>
-          <img src={img3} className={styles.slider_image} alt="" />
-        </div>
-        <div>
-          <img src={img4} className={styles.slider_image} alt="" />
-        </div>
+        {worksList.map((slide, index) => (
+          <a className={styles.slider_link} href={slide.href}>
+            <img src={slide.img} className={styles.slider_image} alt="" />
+            <div className={styles.slider_description}>
+              <div className={styles.slider_description_header}>
+                {slide.header}
+              </div>
+              <div className={styles.slider_description_text}>{slide.text}</div>
+            </div>
+          </a>
+        ))}
       </Slider>
     );
   }
