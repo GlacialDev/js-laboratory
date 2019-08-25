b1bd4cf6c534deca0b2fe529c4803729e246263f
Glacial <kli-van@yandex.ru>
2019-08-25 16:25:10 +0300

    moreAbout part in contacts section. TODO: make mixins and functions in scss

--- src/common/messages/index.js
+++ src/common/messages/index.js
@@ -21,7 +21,20 @@ export default {
     "contacts.phone": "Телефон:",
     "contacts.github": "Гитхаб:",
     "contacts.telegram": "Телеграм:",
-    "contacts.vkontakte": "Вконтакте:"
+    "contacts.vkontakte": "Вконтакте:",
+    "contacts.moreAbout": `Здесь могла быть ваша реклама...
+Шучу. Просто я не знал, что еще можно поместить в секцию с контактами. 
+А без этого блока здесь было бы несколько пустовато, не находите?
+
+В данный момент я ищу работу фронтенд разработчиком. 
+
+Немного о моих личных качествах: 
+    - увлеченный и любознательный
+    - ответственный и работоспособный
+    - умею работать в команде
+    - считаю, что интернет - одно из лучших изобретений человечества (поэтому я веб-разработчик!)
+
+Связаться со мной можно любым из перечисленных слева способов, все ресурсы регулярно просматриваю.`
   },
   en: {
     "header.main": "Main",
@@ -45,6 +58,19 @@ export default {
     "contacts.phone": "Phone:",
     "contacts.github": "Github:",
     "contacts.telegram": "Telegram:",
-    "contacts.vkontakte": "Vkontakte:"
+    "contacts.vkontakte": "Vkontakte:",
+    "contacts.moreAbout": `Your advertisement could be here...
+Just kidding. I just did not know what else could be placed in the contacts section.
+And without this block, it would be somewhat empty here, doesn’t it?
+  
+I'm currently looking for a front-end developer job.
+  
+A little about my personal qualities:
+    - enthusiastic and inquisitive
+    - responsible and efficient
+    - I can work in a team
+    - I think that the Internet is one of the best inventions of mankind (therefore, I am a web developer!)
+
+You can contact me in any of the ways listed on the left, I regularly review all resources.`
   }
 };
--- src/components/About/ContactSection/MoreAbout/MoreAbout.js
+++ src/components/About/ContactSection/MoreAbout/MoreAbout.js
@@ -1,10 +1,13 @@
 import React from "react";
 import styles from "./MoreAbout.module.scss";
+import { FormattedMessage } from "react-intl";
 
 function MoreAbout() {
   return (
     <div className={styles.about}>
-      <div></div>
+      <div className={styles.about_container}>
+        <FormattedMessage id="contacts.moreAbout"></FormattedMessage>
+      </div>
     </div>
   );
 }
