994e77ccf1a4f024067da60f991a6f79227d7f0d
Glacial <kli-van@yandex.ru>
2019-09-01 12:25:21 +0300

    made my forms on Formik

--- src/components/Login/SignInTab/SignInTab.js
+++ src/components/Login/SignInTab/SignInTab.js
@@ -1,81 +1,66 @@
-import React, { Component } from "react";
+import React from "react";
 import styles from "./SignInTab.module.scss";
 import { injectIntl, FormattedMessage } from "react-intl";
+import { Formik, Field, Form } from "formik";
 
-class SignInTab extends Component {
-  state = {
-    values: {
-      nickname: "",
-      password: ""
+function SignInTab({ intl }) {
+  const nicknamePlaceholder = intl.formatMessage({
+    id: "login.nickname_input"
+  });
+  const passwordPlaceholder = intl.formatMessage({
+    id: "login.password_input"
+  });
+  const fields = [
+    {
+      type: "text",
+      name: "nickname",
+      placeholder: nicknamePlaceholder
     },
-    remember: false
-  };
+    {
+      type: "password",
+      name: "password",
+      placeholder: passwordPlaceholder
+    }
+  ];
 
-  handleChange = event => {
-    const { values } = this.state;
-    this.setState({
-      values: { ...values, [event.target.name]: event.target.value }
-    });
-  };
-
-  handleCheckbox = event => {
-    this.setState({ remember: event.target.checked });
-  };
-
-  render() {
-    let { values, remember } = this.state;
-    let { intl } = this.props;
-    const nicknamePlaceholder = intl.formatMessage({
-      id: "login.nickname_input"
-    });
-    const passwordPlaceholder = intl.formatMessage({
-      id: "login.password_input"
-    });
-    const fields = [
-      {
-        type: "text",
-        name: "nickname",
-        placeholder: nicknamePlaceholder
-      },
-      {
-        type: "password",
-        name: "password",
-        placeholder: passwordPlaceholder
-      }
-    ];
-
-    return (
-      <div className={styles.container}>
-        {fields.map(({ type, placeholder, name }, index) => (
-          <input
-            className={styles.input}
-            type={type}
-            placeholder={placeholder}
-            name={name}
-            value={values[name]}
-            onChange={this.handleChange}
-            key={index}
-            required
-          />
-        ))}
-        <div className={styles.checkbox}>
-          <input
-            id="login_checkbox"
-            className={styles.checkbox_input}
-            type="checkbox"
-            checked={remember}
-            onChange={this.handleCheckbox}
-          />
-          <label htmlFor="login_checkbox" className={styles.checkbox_label}>
-            <FormattedMessage id="login.remember" />
-          </label>
-        </div>
-        <button className={styles.button}>
-          <FormattedMessage id="login.login_button" />
-        </button>
-      </div>
-    );
-  }
+  return (
+    <Formik
+      initialValues={{
+        nickname: "",
+        password: ""
+      }}
+      onSubmit={values => {
+        console.log(values);
+      }}
+      render={() => (
+        <Form className={styles.container}>
+          {fields.map(({ type, placeholder, name }, index) => (
+            <Field
+              className={styles.input}
+              type={type}
+              placeholder={placeholder}
+              name={name}
+              key={index}
+              required
+            />
+          ))}
+          <div className={styles.checkbox}>
+            <Field
+              id="login_checkbox"
+              className={styles.checkbox_input}
+              type="checkbox"
+            />
+            <label htmlFor="login_checkbox" className={styles.checkbox_label}>
+              <FormattedMessage id="login.remember" />
+            </label>
+          </div>
+          <button className={styles.button} type="submit">
+            <FormattedMessage id="login.login_button" />
+          </button>
+        </Form>
+      )}
+    />
+  );
 }
 
 export default injectIntl(SignInTab);
--- src/components/Login/SignUpTab/SignUpTab.js
+++ src/components/Login/SignUpTab/SignUpTab.js
@@ -1,94 +1,90 @@
-import React, { Component } from "react";
+import React from "react";
 import styles from "./SignUpTab.module.scss";
 import { injectIntl, FormattedMessage } from "react-intl";
-import { createUser } from "../../../common/api/user";
+// import { createUser } from "../../../common/api/user";
+import { Formik, Field, Form } from "formik";
 
-class SignUpTab extends Component {
-  state = {
-    values: {
-      nickname: "",
-      email: "",
-      password: "",
-      repeatPassword: ""
+function SignUpTab({ intl }) {
+  const nicknamePlaceholder = intl.formatMessage({
+    id: "login.nickname_input"
+  });
+  const emailPlaceholder = intl.formatMessage({
+    id: "login.email_input"
+  });
+  const passwordPlaceholder = intl.formatMessage({
+    id: "login.password_input"
+  });
+  const repeatPasswordPlaceholder = intl.formatMessage({
+    id: "login.repeat_password_input"
+  });
+  const fields = [
+    {
+      type: "text",
+      name: "nickname",
+      placeholder: nicknamePlaceholder
+    },
+    {
+      type: "email",
+      name: "email",
+      placeholder: emailPlaceholder
+    },
+    {
+      type: "password",
+      name: "password",
+      placeholder: passwordPlaceholder
+    },
+    {
+      type: "password",
+      name: "repeatPassword",
+      placeholder: repeatPasswordPlaceholder
     }
-  };
+  ];
 
-  handleChange = event => {
-    const { values } = this.state;
-    this.setState({
-      values: { ...values, [event.target.name]: event.target.value }
-    });
-  };
-
-  handleButton = async event => {
-    const { nickname, email, password } = this.state.values;
-    let req = await createUser("/api/user/create", {
-      nickname,
-      email,
-      password
-    });
-
-    console.log(req);
-  };
-
-  render() {
-    let { values } = this.state;
-    let { intl } = this.props;
-    const nicknamePlaceholder = intl.formatMessage({
-      id: "login.nickname_input"
-    });
-    const emailPlaceholder = intl.formatMessage({
-      id: "login.email_input"
-    });
-    const passwordPlaceholder = intl.formatMessage({
-      id: "login.password_input"
-    });
-    const repeatPasswordPlaceholder = intl.formatMessage({
-      id: "login.repeat_password_input"
-    });
-    const fields = [
-      {
-        type: "text",
-        name: "nickname",
-        placeholder: nicknamePlaceholder
-      },
-      {
-        type: "email",
-        name: "email",
-        placeholder: emailPlaceholder
-      },
-      {
-        type: "password",
-        name: "password",
-        placeholder: passwordPlaceholder
-      },
-      {
-        type: "password",
-        name: "repeatPassword",
-        placeholder: repeatPasswordPlaceholder
-      }
-    ];
-
-    return (
-      <div className={styles.container}>
-        {fields.map(({ type, placeholder, name }, index) => (
-          <input
-            className={styles.input}
-            type={type}
-            placeholder={placeholder}
-            name={name}
-            value={values[name]}
-            onChange={this.handleChange}
-            key={index}
-            required
-          />
-        ))}
-        <button className={styles.button} onClick={this.handleButton}>
-          <FormattedMessage id="login.register_button" />
-        </button>
-      </div>
-    );
-  }
+  return (
+    // {fields.map(({ type, placeholder, name }, index) => (
+    //     <input
+    //       className={styles.input}
+    //       type={type}
+    //       placeholder={placeholder}
+    //       name={name}
+    //       value={values[name]}
+    //       onChange={this.handleChange}
+    //       key={index}
+    //       required
+    //     />
+    //   ))}
+    //   <button className={styles.button} onClick={this.handleButton}>
+    //     <FormattedMessage id="login.register_button" />
+    //   </button>
+    <Formik
+      initialValues={{
+        nickname: "",
+        email: "",
+        password: "",
+        repeatPassword: ""
+      }}
+      onSubmit={values => {
+        console.log(values);
+      }}
+      render={() => (
+        <Form className={styles.container}>
+          {fields.map(({ type, placeholder, name }, index) => (
+            <Field
+              className={styles.input}
+              type={type}
+              placeholder={placeholder}
+              name={name}
+              key={index}
+              required
+            />
+          ))}
+          <button className={styles.button} type="submit">
+            <FormattedMessage id="login.register_button" />
+          </button>
+        </Form>
+      )}
+    />
+  );
 }
 
 export default injectIntl(SignUpTab);
