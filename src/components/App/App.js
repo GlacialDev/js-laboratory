4fed4d4e3734baa98bf0a9b757f676a6d19963a6
Glacial <kli-van@yandex.ru>
2019-09-09 18:54:18 +0300

    didn't commitet with last commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,46 +2,12 @@ import React, { Component } from "react";
 import Main from "./Main";
 import { IntlProvider } from "react-intl";
 import messages from "../../common/messages";
-import {
-  signInJWTRequest,
-  refreshAccessTokenRequest
-} from "../../common/api/user";
-import jwt_decode from "jwt-decode";
 
 import "normalize.css";
 import "../../common/styles/universal.scss";
 import styles from "./App.module.scss";
 
 class App extends Component {
-  componentDidMount() {
-    const { context } = this.props;
-    const { accessToken, refreshToken } = localStorage;
-
-    if (accessToken && refreshToken) {
-      const decodedAccessToken = jwt_decode(accessToken);
-      let timeNow = new Date().getTime() / 1000;
-      let accessTokenLifetimeEnd = new Date(decodedAccessToken.exp).getTime();
-
-      if (accessTokenLifetimeEnd < timeNow) {
-        refreshAccessTokenRequest(refreshToken).then(response => {
-          this.saveNewTokenPair(response);
-          signInJWTRequest(localStorage.accessToken).then(response => {
-            this.signIn(response, context);
-          });
-        });
-      } else {
-        signInJWTRequest(accessToken).then(response => {
-          this.signIn(response, context);
-        });
-      }
-    }
-  }
-
-  saveNewTokenPair(response) {
-    localStorage.accessToken = response.answer.accessToken;
-    localStorage.refreshToken = response.answer.refreshToken;
-  }
-
   signIn(response, context) {
     const { nickname, email, isAuth } = response.answer;
     context.setNickname(nickname);
